0baa433fcd992021340fdad9a0d4f2a41e6d5072
thessasilviana <46558965+thessasilviana@users.noreply.github.com>
2019-05-20 00:21:43 +0700

    Add files via upload

---
+++ C-macbookpro2.java
@@ -0,0 +1,82 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class MacbookPro {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testMacbookPro() throws Exception {
+    driver.get("https://www.amazon.com/");
+    driver.findElement(By.id("twotabsearchtextbox")).click();
+    driver.findElement(By.id("twotabsearchtextbox")).clear();
+    driver.findElement(By.id("twotabsearchtextbox")).sendKeys("macbook pro");
+    driver.findElement(By.name("site-search")).submit();
+    driver.findElement(By.id("a-autoid-0-announce")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='›'])[1]/following::img[1]")).click();
+    driver.findElement(By.id("a-autoid-0-announce")).click();
+    driver.findElement(By.id("a-autoid-0-announce")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Qty:'])[1]/following::span[4]")).click();
+    driver.findElement(By.id("quantity_1")).click();
+    driver.findElement(By.id("add-to-cart-button")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
